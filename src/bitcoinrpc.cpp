a848c82242db77cf490ee96940cce1ca457718ad
Palm Detroit <palmdetroit@gmail.com>
2014-12-01 22:46:14 -0500

    Remove Loop

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -2544,7 +2544,7 @@ int ReadHTTPStatus(std::basic_istream<char>& stream, int &proto)
 int ReadHTTPHeader(std::basic_istream<char>& stream, map<string, string>& mapHeadersRet)
 {
     int nLen = 0;
-    loop
+    while (true)
     {
         string str;
         std::getline(stream, str);
@@ -3083,7 +3083,7 @@ void ThreadRPCServer3(void* parg)
     AcceptedConnection *conn = (AcceptedConnection *) parg;
 
     bool fRun = true;
-    loop {
+    while (true) {
         if (fShutdown || !fRun)
         {
             conn->close();
